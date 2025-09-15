8dba2819dc3f3bb33c39d9b004699703a567e787
Алина <degtyareva_18@mail.ru>
2025-09-15 10:44:07 +0300

    update

---
+++ Try catch с файлами/ConsoleApp9/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApp9
+{
+    internal class Program
+    {
+        static void Main()
+        {
+
+            try
+            {
+                Console.Write("Введите путь к файлу: ");
+                string path = Console.ReadLine();
+                if (!File.Exists(path))
+                {
+                    Console.WriteLine("Данный файл не существует");
+                    return;
+                }
+                string text = File.ReadAllText(path);
+                Console.WriteLine("Содержимое файла: ");
+                Console.WriteLine(text);
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine("Неверный путь файла, " + ex.Message);
+            }
+            catch (DirectoryNotFoundException ex)
+            {
+                Console.WriteLine("Директория не найдена, " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Непредвиденная ошибка, " + ex.Message);
+            }
+
+            finally
+            {
+                Console.WriteLine("Нажмите Esc чтобы выйти");
+                while (Console.ReadKey(true).Key != ConsoleKey.Escape) { }
+            }
+            
+        }
+
+    }
+}
---
+++ Try catch с файлами/ConsoleApp9/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("ConsoleApp9")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ConsoleApp9")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("5d3df3fa-b1a1-4c65-901a-c9bea6c22d8f")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
